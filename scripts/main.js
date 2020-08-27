d377976f08b0218118a67508e639c630fa57cbd6
John Lynch <johnlynch.32768@gmail.com>
2020-08-27 02:27:58 +0100

    Add yet another milestone.

--- scripts/main.js
+++ scripts/main.js
@@ -5,7 +5,7 @@ document.body.onload = _ => {
     const now_box = document.getElementById(`now`);
     flatpickr(`#datob`, {enableTime: true});
     divisors = [1, 60, 3600, 86400, 604800, 2624832, 31557600]
-    milestones = [~~5e8, ~~1e9, ~~15e8, ~~2e9, ~~6e8, ~~9e8, 864e6, 72e7];
+    milestones = [5e8, 1e9, 15e8, 2e9, 6e8, 9e8, 18e8, 864e6, 72e7];
     dtadd = (dt, secs) => {
         dtc = new Date(dt);
         dtc.setSeconds(dtc.getSeconds() + secs);
@@ -13,10 +13,8 @@ document.body.onload = _ => {
     };
     input.addEventListener(`change`, ev => {
         const now = new Date();
-        now_box.textContent = now.toString().slice(0, -30);
-        
-        // .toISOString().slice(0, -5).replace(`T`, ` `);
-        then = new Date(input.value);3
+        now_box.textContent = get_rid_of_shit(now);
+        then = new Date(input.value);
         set_values(then, now);
         if (typeof counter != `undefined`) clearInterval(counter);
         counter = setInterval(_ => {
@@ -27,9 +25,14 @@ document.body.onload = _ => {
 
     function set_values(t0, t1) {
         const secs = ~~[(t1 - t0) / 1000];
-        now_box.textContent = new Date().toString().slice(0, -30);
+        now_box.textContent = get_rid_of_shit(new Date());
         for (const [i, val] of [...outputs].entries()) {
-            val.value = i < 7 ? (~~(secs / divisors[i])).toLocaleString() : dtadd(t0, milestones[i - 7]).toString().slice(0, -30);
+            val.value = i < 7 ? (~~(secs / divisors[i])).toLocaleString() : get_rid_of_shit(dtadd(t0, milestones[i - 7]));
         }
     }
+
+    function get_rid_of_shit(ridiculously_long_date_string_object) {
+        return ridiculously_long_date_string_object.toString().slice(0, -30);
+    }
 };
+
